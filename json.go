d08b5d59cf2e611a76a2471f32a412fcc2cd2742
The Greatest Giant <seanalter1@gmail.com>
2023-09-24 16:42:06 -0400

    Boot.dev 5.3
    Fix gosec errors

--- json.go
+++ json.go
@@ -27,5 +27,9 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 		return
 	}
 	w.WriteHeader(code)
-	w.Write(dat)
+	_, err = w.Write(dat)
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
 }
